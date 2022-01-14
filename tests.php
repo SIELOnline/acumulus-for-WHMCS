194926734c126ea7ec6bcfcea5b3928d2536a7ab
Erwin Derksen <erwin@burorader.com>
2022-01-14 14:18:47 +0100

    Added test data, may be used in the future

---
+++ tests.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Ingelogd als: erwin@burorader.com
+ * Besteld: erwinderksen.com hosting + domein
+ */
+$varsJson1 = '{"source":"autogen","user":"system","invoiceid":1,"status":"Unpaid"}';
+// http://whmcs-acumulus.loc/viewinvoice.php?id=1
+// http://whmcs-acumulus.loc/modules/addons/acumulus_connect/test/Factuur-WHA20221.pdf
+/*
+ * Blijkbaar ging er iets fout want het proces werd opnieuw uitgevoerd en er
+ * zijn nu 2 onbetaalde facturen.
+ * Ingelogd als: erwin@burorader.com
+ * Besteld: erwinderksen.com hosting + domein
+ */
+$varsJson2 = '{"source":"autogen","user":"system","invoiceid":2,"status":"Unpaid"}';
+// http://whmcs-acumulus.loc/viewinvoice.php?id=2
+// http://whmcs-acumulus.loc/modules/addons/acumulus_connect/test/Factuur-WHA20222.pdf
+$varsJson3 = '{"source":"autogen","user":"system","invoiceid":2}';
